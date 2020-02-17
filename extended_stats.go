f8292ce5109e5d20211de958e0ad8fdbd056f6b9
Yang Bo <bo@hyper.sh>
2020-02-17 20:46:03 +0800

    extended_stats: add function implement
    Signed-off-by: Yang Bo <bo@hyper.sh>

--- extended_stats.go
+++ extended_stats.go
@@ -149,13 +149,192 @@ func (c *container) GetContainerStats() (*pb.ContainerStats, error) {
 	}, nil
 }
 
+// ajdk_score come from environment of init process
 func (c *container) GetContainerBaseStats() (*pb.ContainerBaseStats, error) {
 	ContainerId := c.container.ID()
 
 	// get cgroup paths and then read out info from cgroup fs
 	state, err := c.container.State()
 	cgroups := state.CgroupPaths
+
+	CgroupCpu, err := getContainerCgroupCpu(cgroups)
+	if err != nil {
+		return nil, err
+	}
+
+	// get ajdk_score
+	CgroupCpu.JdkMaxProcessLimit = c.getAjdkScore()
+
+	CgroupCpushare, err := getContainerCgroupCpushare(cgroups)
+	if err != nil {
+		return nil, err
+	}
+
+	CgroupIo, err := getContainerCgroupIo(cgroups)
+	if err != nil {
+		return nil, err
+	}
+
+	CgroupIotime, err := getContainerCgroupIoTime(cgroups)
+	if err != nil {
+		return nil, err
+	}
+
+	CgroupIox, err := getContainerCgroupIox(cgroups)
+	if err != nil {
+		return nil, err
+	}
+
+	CgroupLoad, err := getContainerCgroupLoad(cgroups)
+	if err != nil {
+		return nil, err
+	}
+
+	CgroupMem, err := getContainerCgroupMem(cgroups)
+	if err != nil {
+		return nil, err
+	}
+
+	CgroupMemdelay, err := getContainerCgroupMemDelay(cgroups)
+	if err != nil {
+		return nil, err
+	}
+
+	CgroupLatencyStat, err := getContainerCgroupLatencyStats(cgroups)
+	if err != nil {
+		return nil, err
+	}
+
+	CgroupMemx, err := getContainerCgroupMemx(cgroups)
+	if err != nil {
+		return nil, err
+	}
+
+	CgroupPcsw, err := getContainerCgroupPcsw(cgroups)
+	if err != nil {
+		return nil, err
+	}
+
+	CgroupPercpu, err := getContainerCgroupPercpu(cgroups)
+	if err != nil {
+		return nil, err
+	}
+
+	CgroupSchedbvt, err := getContainerCgroupSchedbvt(cgroups)
+	if err != nil {
+		return nil, err
+	}
+
+	CgroupSchedcg, err := getContainerCgroupSchedcg(cgroups)
+	if err != nil {
+		return nil,err
+	}
+
+	CgroupTask, err := getContainerCgroupTask(cgroups)
+	if err != nil {
+		return nil, err
+	}
+
+	CgroupTcpStats, err := getContainerCgroupTcpStats(cgroups)
+	if err != nil {
+		return nil, err
+	}
+
+	CgroupTcpxStats, err := getContainerCgroupTcpxStats(cgroups)
+	if err != nil {
+		return nil, err
+	}
+
+	CgroupTrafficStats, err := getContainerCgroupTrafficStats(cgroups)
+	if err != nil {
+		return nil, err
+	}
+
+	CgroupUdpStats, err := getContainerCgroupUdpStats(cgroups)
+	if err != nil {
+		return nil, err
+	}
+
+	CgroupVm, err := getContianerCgroupVm(cgroups)
+	if err != nil {
+		return nil, err
+	}
+
+	PartitionStats, err := getContainerPartitionStats(cgroups)
+	if err != nil {
+		return nil, err
+	}
+
+	return &ContainerBaseStats {
+		ContainerId,
+		CgroupCpu,
+		CgroupCpushare,
+		CgroupIo,
+		CgroupIotime,
+		CgroupIox,
+		CgroupLoad,
+		CgroupMem,
+		CgroupMemdelay,
+		CgroupLatencyStat,
+		CgroupMemx,
+		CgroupPcsw,
+		CgroupPercpu,
+		CgroupSchedbvt,
+		CgroupSchedcg,
+		CgroupTask,
+		CgroupTcpStats,
+		CgroupTcpxStats,
+		CgroupTafficStats,
+		CgroupUdpStats,
+		CgroupVm,
+		PartitionStats,
+	}, nil
 }
 
 func (p *process) GetProcessStats() (*pb.ProcessStats, error) {
+	Pid := p.process.Pid()
+
+	PidStats, err := getProcessPidStats(Pid)
+	if err != nil {
+		return nil, err
+	}
+
+	CgroupSched, errr := getProcessCgroupSched(Pid)
+	if err != nil {
+		return nil, err
+	}
+
+	ProcCpuStats, err := getProcessProcCpuStats(Pid)
+	if err != nil {
+		return nil, err
+	}
+
+	ProcIoStats, err := getProcessProcIoStats(Pid)
+	if err != nil {
+		return nil, err
+	}
+
+	ProcMemStats, err := getProcessProcMemStats(Pid)
+	if err != nil {
+		return nil, err
+	}
+
+	return &ProcessStats {
+		Pid,
+		PidStats,
+		CgroupSched,
+		ProcCpuStats,
+		ProcIoStats,
+		ProcMemStats,
+	}, nil
+}
+
+const (
+	aliProcPath = "/sys/fs/cgroup/cpuacct/cpuacct.proc_stat"
+)
+
+// true if ali kernel, false on other kernels
+func aliKernel() (bool) {
+	_, err := os.Stat(aliProcPath)
+	return err == nil || os.IsExist(err)
 }
